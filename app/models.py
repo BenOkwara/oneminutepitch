9bb51b0cf30b66cbb9ca09ccf87db24d5062aafa
melisamalala <melissamalala@gmail.com>
2018-09-07 11:29:33 +0300

    Created registration form for user login

--- app/models.py
+++ app/models.py
@@ -1,11 +1,32 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255), index = True)
+    email = db.Column(db.String(255), unique=True, index=True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
     def __repr__(self):
@@ -20,26 +41,26 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'User {self.name}'
-
-
-class Pitch:
-
-    all_pitches = []
-
-    def __init__(self,title,pitch):
-        # self.movie_id = movie_id
-        self.title = title
-        # self.imageurl = imageurl
-        self.pitch = pitch
-
-
-    def save_pitch(self):
-        Pitch.all_pitches.append(self)
-
-
-    @classmethod
-    def clear_pitch(cls):
-        Pitch.all_pitches.clear()
-
-
-
+#
+#
+# class Pitch:
+#
+#     all_pitches = []
+#
+#     def __init__(self,title,pitch):
+#         # self.movie_id = movie_id
+#         self.title = title
+#         # self.imageurl = imageurl
+#         self.pitch = pitch
+#
+#
+#     def save_pitch(self):
+#         Pitch.all_pitches.append(self)
+#
+#
+#     @classmethod
+#     def clear_pitch(cls):
+#         Pitch.all_pitches.clear()
+#
+#
+#
