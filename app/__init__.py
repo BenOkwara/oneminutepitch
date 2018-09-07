4fb4f1b68ef6477ca3795655a84e641d43ce78ef
melisamalala <melissamalala@gmail.com>
2018-09-07 12:24:53 +0300

    Configured email and welcome email file

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -12,6 +13,8 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,10 +22,14 @@ def create_app(config_name):
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
+    # configure UploadSet
+    configure_uploads(app, photos)
+
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
