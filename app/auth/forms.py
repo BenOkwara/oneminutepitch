a52eca80a025ba3d3fe0f5003877b38dcee546af
melisamalala <melissamalala@gmail.com>
2018-09-07 11:50:58 +0300

    Created logout section

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField, BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
@@ -12,8 +12,6 @@ class RegistrationForm(FlaskForm):
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
 
-class RegistrationForm(FlaskForm):
-    # .......
     def validate_email(self,data_field):
             if User.query.filter_by(email =data_field.data).first():
                 raise ValidationError('There is an account with that email')
@@ -21,3 +19,10 @@ class RegistrationForm(FlaskForm):
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
